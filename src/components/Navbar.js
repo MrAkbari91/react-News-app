9ebb5dcd255260b904f1a8a32cb356e0de0298bc
Dhruv Akbari <83423967+MrAkbari91@users.noreply.github.com>
2023-02-16 10:43:30 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export class Navbar extends Component {
         let { logo, navitem, theme, toggleDarkMode } = this.props
         return (
             <div>
-                <nav class='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
+                <nav class='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 fixed w-full z-10'>
                     <div class='container flex flex-wrap items-center justify-between mx-auto'>
                         <Link to='https://flowbite.com/' className='flex items-center'>
                             <span className='self-center text-3xl whitespace-nowrap dark:text-white logo'>{logo}</span>
